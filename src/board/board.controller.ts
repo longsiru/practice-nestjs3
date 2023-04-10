2fb7790f86ca08afe7bb62dc4b880a264d95f706
longsiru <sirulong89@gmail.com>
2023-04-10 18:59:32 +0900

    post board

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Board } from './../../dist/board/board.model.d';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardService } from './board.service';
+import { title } from 'process';
 
 @Controller('boards')
 export class BoardController {
@@ -7,7 +9,16 @@ export class BoardController {
   //private 사용하는 이유는 파라미터에서 암목족으로 프로퍼티로 바뀌게 되어서 앞으로 퍼티로선언되어서 바로 이 클레스안에서 this를 이용할 수 있다.
   constructor(private boardService: BoardService) {}
   @Get('/')
-  getAllBoard() {
+  getAllBoard(): Board[] {
+    //array
     return this.boardService.getAllBoards();
+  }
+
+  @Post()
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Board {
+    return this.boardService.createBoard(title, description);
   }
 }
--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,10 +1,24 @@
+import { Board, BoardStatus } from './board.model';
 import { Injectable } from '@nestjs/common';
+import { v1 as uuid } from 'uuid'; //uuid：npm install uuid --save。。v1是uuid的版本，我们用uuid这个名字来呼出。
 
 @Injectable()
 export class BoardService {
-  private boards = [];
+  private boards: Board[] = [];
 
-  getAllBoards() {
-    return this.boards;
+  getAllBoards(): Board[] {
+    return this.boards; //return 값 영러개 있으니까, type도 array으로 구성 해야한다.
+  }
+
+  createBoard(title: string, description: string) {
+    const board: Board = {
+      //postman로 시행했다.
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+    this.boards.push(board); //我们做了这个board，要用push把这个数据放到我们的boards数组中。
+    return board; //return를 해서 board어떤한 title,description사용한 게시물이 만들어는지 이정보를 return해줘.
   }
 }
